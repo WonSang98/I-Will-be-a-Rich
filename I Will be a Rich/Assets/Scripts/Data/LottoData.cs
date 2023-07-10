a5777f316da4df63216d21366fc3b014b08822aa
WonSang98 <cdsywon@kookmin.ac.kr>
2023-07-10 15:28:33 +0900

    feat : LottoData 추가
    LottoData 가 추가되었습니다.
    
    해당 데이터는 Lotto의 비용, 등수 별 상금 등의 데이터가 정리되어있습니다.
    
    DataManager를 통해 해당 데이터를 불러 온 후 게임에 값을 적용합니다.

---
+++ I Will be a Rich/Assets/Scripts/Data/LottoData.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using static Define;
+
+public class LottoData
+{
+    [XmlAttribute]
+    public int ID;
+    [XmlAttribute]
+    public LottoType type;
+    [XmlAttribute]
+    public int nameID;
+    [XmlAttribute]
+    public int price;
+    [XmlAttribute]
+    public float coolTime;
+    [XmlAttribute]
+    public int max;
+    [XmlAttribute]
+    public int prize1;
+    [XmlAttribute]
+    public int prize2;
+    [XmlAttribute]
+    public int prize3;
+    [XmlAttribute]
+    public int prize4;
+    [XmlAttribute]
+    public int prize5;
+}
+
+[Serializable, XmlRoot("ArrayOfStatData")]
+public class LottoDataLoader : ILoader<int, LottoData>
+{
+    [XmlElement("StatData")]
+    public List<LottoData> _lottoData = new List<LottoData>();
+
+    public Dictionary<int, LottoData> MakeDic()
+    {
+        Dictionary<int, LottoData> dic = new Dictionary<int, LottoData>();
+
+        foreach (LottoData data in _lottoData)
+            dic.Add(data.ID, data);
+
+        return dic;
+    }
+
+    public bool Validate()
+    {
+        return true;
+    }
+}
