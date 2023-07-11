dafe86dc206f13b8bdf9a2bb689ad50d096dd0aa
WonSang98 <cdsywon@kookmin.ac.kr>
2023-07-11 14:58:05 +0900

    fix : Lotto Item View 벗어나는 문제 해결
    기존 LottoItem 스크립트가 UI_Popup을 상속받아 캔버스를 생성하여 view를 벗어나는 문제를 해결하였습니다.

--- I Will be a Rich/Assets/Scripts/UI/Popup/UI_LottoItem.cs
+++ I Will be a Rich/Assets/Scripts/UI/Popup/UI_LottoItem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UI_LottoItem : UI_Popup
+public class UI_LottoItem : UI_Base
 {
     enum Texts
     {
